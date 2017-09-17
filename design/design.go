bf0ffe6c7a0ca9f2d3a2aaeb9cdca7b6752ba463
Masaki Yoshida <yoshida.masaki+restartr@gmail.com>
2017-09-18 08:58:29 +0900

    Define first API design

---
+++ design/design.go
@@ -0,0 +1,43 @@
+package design
+
+import (
+	. "github.com/goadesign/goa/design"
+	. "github.com/goadesign/goa/design/apidsl"
+)
+
+var _ = API("celler", func() {
+	Title("The virtual wine celler")
+	Description("A simple goa service")
+	Scheme("http")
+	Host("localhost:8000")
+})
+
+var _ = Resource("bottle", func() {
+	BasePath("/bottles")
+	DefaultMedia(BottleMedia)
+
+	Action("show", func() {
+		Description("Get bottle by id")
+		Routing(GET("/:bottleID"))
+		Params(func() {
+			Param("bottleID", Integer, "Bottle ID")
+		})
+		Response(OK)
+		Response(NotFound)
+	})
+})
+
+var BottleMedia = MediaType("application/vnd.goa.example.com+json", func() {
+	Description("A bottle of wine")
+	Attributes(func() {
+		Attribute("id", Integer, "Unique bottle ID")
+		Attribute("href", String, "API href for making requests on the bottle")
+		Attribute("name", String, "Name of wine")
+		Required("id", "href", "name")
+	})
+	View("default", func() {
+		Attribute("id")
+		Attribute("href")
+		Attribute("name")
+	})
+})
